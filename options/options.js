9b9a255b8a7802370ffaab1d873c206e5e934327
Truman Ellis <truman.e.ellis@gmail.com>
2023-03-29 14:35:14 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "gentle-field-39612",
-  url: "https://gentle-field-39612.botics.co",
-  api: "https://gentle-field-39612.botics.co/api/v1"
-}
+  "name": "gentle-field-39612",
+  "url": "https://gentle-field-39612.botics.co",
+  "api": "https://gentle-field-39612.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
